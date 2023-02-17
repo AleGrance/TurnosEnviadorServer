f61b780458813f0ad512f12bcc15794ca6d7da0e
Tecnico <Tecnico@NB-INFOR-001006.FUTURO.LOCAL>
2023-02-17 10:39:42 -0300

    add turnosNotificadosFecha appRoute

--- src/routes/turnos.js
+++ src/routes/turnos.js
@@ -84,6 +84,47 @@ module.exports = (app) => {
       });
   });
 
+  // Trae la cantidad de turnos enviados por rango de fecha desde hasta
+  app.route("/turnosNotificadosFecha").post((req, res) => {
+    let fechaHoy = new Date().toISOString().slice(0, 10);
+    let {fecha_desde, fecha_hasta} = req.body
+
+    if(fecha_desde === "" && fecha_hasta === "") {
+      fecha_desde = fechaHoy;
+      fecha_hasta = fechaHoy;
+    }
+
+    if(fecha_hasta == "") {
+      fecha_hasta = fecha_desde;
+    }
+
+    if(fecha_desde == "") {
+      fecha_desde = fecha_hasta;
+    }
+
+    console.log(req.body);
+
+    Turnos.count({
+      where: {
+        [Op.and]: [
+          { estado_envio: 1 },
+          {
+            updatedAt: {
+              [Op.between]: [fecha_desde + " 00:00:00", fecha_hasta + " 23:59:59"],
+            },
+          },
+        ],
+      },
+      //order: [["createdAt", "DESC"]],
+    })
+      .then((result) => res.json(result))
+      .catch((error) => {
+        res.status(402).json({
+          msg: error.menssage,
+        });
+      });
+  });
+
   // Turnos no enviados - estado_envio 2 o 3
   app.route("/turnosNoNotificados").get((req, res) => {
     Turnos.findAll({
@@ -142,62 +183,62 @@ module.exports = (app) => {
 
   function injeccionFirebird() {
     console.log("Se actualiza el PSQL");
-    Firebird.attach(odontos, function (err, db) {
-      if (err) throw err;
+    // Firebird.attach(odontos, function (err, db) {
+    //   if (err) throw err;
 
-      // db = DATABASE
-      db.query(
-        // Trae los ultimos 50 registros de turnos del JKMT
-        "SELECT * FROM VW_RESUMEN_TURNOS_HOY ROWS 50",
-        //"SELECT COUNT(*) FROM VW_RESUMEN_TURNOS_HOY",
-        function (err, result) {
-          console.log("Cant de turnos obtenidos del JKMT:", result.length);
+    //   // db = DATABASE
+    //   db.query(
+    //     // Trae los ultimos 50 registros de turnos del JKMT
+    //     "SELECT * FROM VW_RESUMEN_TURNOS_HOY ROWS 50",
+    //     //"SELECT COUNT(*) FROM VW_RESUMEN_TURNOS_HOY",
+    //     function (err, result) {
+    //       console.log("Cant de turnos obtenidos del JKMT:", result.length);
 
-          // Recorre el array que contiene los datos e inserta en la base de postgresql
-          result.forEach((e) => {
-            // Si el nro de cert trae NULL cambiar por 000000
-            if (!e.NRO_CERT) {
-              e.NRO_CERT = " ";
-            }
-            // Si no tiene plan
-            if (!e.PLAN_CLIENTE) {
-              e.PLAN_CLIENTE = " ";
-            }
-            // Si la hora viene por ej: 11:0 entonces agregar el 0 al final
-            if (e.HORA[3] === "0") {
-              e.HORA = e.HORA + "0";
-            }
-            // Si la hora viene por ej: 10:3 o 11:2 entonces agregar el 0 al final
-            if (e.HORA.length === 4 && e.HORA[0] === "1") {
-              e.HORA = e.HORA + "0";
-            }
-            // Si el nro de tel trae NULL cambiar por 595000 y cambiar el estado a 2
-            // Si no reemplazar el 0 por el 595
-            if (!e.TELEFONO_MOVIL) {
-              e.TELEFONO_MOVIL = "595000";
-              e.estado_envio = 2;
-            } else {
-              e.TELEFONO_MOVIL = e.TELEFONO_MOVIL.replace(0, "595");
-            }
+    //       // Recorre el array que contiene los datos e inserta en la base de postgresql
+    //       result.forEach((e) => {
+    //         // Si el nro de cert trae NULL cambiar por 000000
+    //         if (!e.NRO_CERT) {
+    //           e.NRO_CERT = " ";
+    //         }
+    //         // Si no tiene plan
+    //         if (!e.PLAN_CLIENTE) {
+    //           e.PLAN_CLIENTE = " ";
+    //         }
+    //         // Si la hora viene por ej: 11:0 entonces agregar el 0 al final
+    //         if (e.HORA[3] === "0") {
+    //           e.HORA = e.HORA + "0";
+    //         }
+    //         // Si la hora viene por ej: 10:3 o 11:2 entonces agregar el 0 al final
+    //         if (e.HORA.length === 4 && e.HORA[0] === "1") {
+    //           e.HORA = e.HORA + "0";
+    //         }
+    //         // Si el nro de tel trae NULL cambiar por 595000 y cambiar el estado a 2
+    //         // Si no reemplazar el 0 por el 595
+    //         if (!e.TELEFONO_MOVIL) {
+    //           e.TELEFONO_MOVIL = "595000";
+    //           e.estado_envio = 2;
+    //         } else {
+    //           e.TELEFONO_MOVIL = e.TELEFONO_MOVIL.replace(0, "595");
+    //         }
 
-            // Reemplazar por mi nro para probar el envio
-            // if (!e.TELEFONO_MOVIL) {
-            //   e.TELEFONO_MOVIL = "595000";
-            //   e.estado_envio = 2;
-            // } else {
-            //   e.TELEFONO_MOVIL = "595986153301";
-            // }
+    //         // Reemplazar por mi nro para probar el envio
+    //         // if (!e.TELEFONO_MOVIL) {
+    //         //   e.TELEFONO_MOVIL = "595000";
+    //         //   e.estado_envio = 2;
+    //         // } else {
+    //         //   e.TELEFONO_MOVIL = "595986153301";
+    //         // }
 
-            Turnos.create(e)
-              //.then((result) => res.json(result))
-              .catch((error) => console.log(error.message));
-          });
+    //         Turnos.create(e)
+    //           //.then((result) => res.json(result))
+    //           .catch((error) => console.log(error.message));
+    //       });
 
-          // IMPORTANTE: cerrar la conexion
-          db.detach();
-        }
-      );
-    });
+    //       // IMPORTANTE: cerrar la conexion
+    //       db.detach();
+    //     }
+    //   );
+    // });
   }
 
   setInterval(() => {
